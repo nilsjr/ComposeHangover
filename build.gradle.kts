e4d54015f89abce0586935e1664445b1ea2ea9b3
Nils Druyen <nils.druyen@fressnapf.com>
2023-03-02 21:48:27 +0100

    chore: update kmp coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.android.application") version "8.0.0-beta04" apply false
     id("com.android.library") version "8.0.0-beta04" apply false
-    id("org.jetbrains.compose") version "1.3.0-beta04-dev885" apply false
+    id("org.jetbrains.compose") version "1.4.0-alpha01-dev958" apply false
     id("com.github.ben-manes.versions") version "0.46.0"
-    id("com.rickclephas.kmp.nativecoroutines") version "0.13.1" apply false
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("com.rickclephas.kmp.nativecoroutines") version "1.0.0-ALPHA-5" apply false
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
     id("com.google.cloud.tools.appengine") version("2.4.5") apply false
 }
 
