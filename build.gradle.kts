36c7655a07eec44a022bc12a1e6af50fa2c8293f
Nils Druyen <nils.druyen@fressnapf.com>
2023-09-09 20:32:19 +0200

    chore: update deps & gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("multiplatform") version "1.9.0" apply false
     kotlin("android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
-    id("com.android.application") version "8.2.0-alpha14" apply false
-    id("com.android.library") version "8.2.0-alpha14" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.android.application") version "8.2.0-beta02" apply false
+    id("com.android.library") version "8.2.0-beta02" apply false
     alias(libs.plugins.jetbrains.compose) apply false
-    alias(libs.plugins.gradle.versions) apply false
-    id("com.rickclephas.kmp.nativecoroutines") version "1.0.0-ALPHA-5" apply false
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    alias(libs.plugins.gradle.versions)
+    id("com.rickclephas.kmp.nativecoroutines") version "1.0.0-ALPHA-18" apply false
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("com.google.cloud.tools.appengine") version("2.4.5") apply false
 }
 
